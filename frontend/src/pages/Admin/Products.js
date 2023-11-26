778b108399c8d21ac4c736af7fb891d97ea7537d
Ifham <ifhamhusain778866@gmail.com>
2023-11-27 00:04:40 +0530

    Containerized the App

--- frontend/src/pages/Admin/Products.js
+++ frontend/src/pages/Admin/Products.js
@@ -10,7 +10,7 @@ const Products = () => {
   //getall products
   const getAllProducts = async () => {
     try {
-      const { data } = await axios.get("/api/v1/product/get-product");
+      const { data } = await axios.get("http://localhost:8080/api/v1/product/get-product");
       setProducts(data.products);
     } catch (error) {
       console.log(error);
@@ -43,7 +43,7 @@ const Products = () => {
                   style={{ width: "18rem" }}
                 >
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
